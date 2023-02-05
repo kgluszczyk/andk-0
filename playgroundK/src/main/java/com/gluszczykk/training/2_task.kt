0f1223c2b67e03128c579e8223929cb2219b3113
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2023-02-05 23:45:25 +0100

    Cwieczenie 2

---
+++ playgroundK/src/main/java/com/gluszczykk/training/2_task.kt
@@ -0,0 +1,19 @@
+package com.gluszczykk.training
+
+/**
+1. Napisz wyrażenie if/else, które wypisze "No to się wysypia" jeśli nie ma dzieci, i "Oj, będzie cieżkow" jeśli ma
+
+2. Napisz wyrażenie when, które przypisze do zmiennej "Nastolatek" jesli wiek jest pomiedzy 13-19, 'dorosły' 20-59 i "Senior" jeśli jest powyżej 60
+
+3. Napisz pętle for, która wypisze wszystkie lata przestepne pomiedzy 2010 a 2020
+
+4. Napisz pętle while, która wpisze do osobnej tablicy 5 pierwszych lat przestepnych
+
+5. Napisz pętle do-while, która wypisze wszystkie lata z talbicy z pkt.4 do 2008 włącznie
+
+
+ **/
+
+fun main(){
+
+}
