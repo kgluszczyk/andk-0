d706f5cdc85f39b692509b32fce889ba40b0f62f
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2023-02-05 23:43:51 +0100

    Cwieczenie 1

---
+++ playgroundK/src/main/java/com/gluszczykk/training/1_task.kt
@@ -0,0 +1,15 @@
+package com.gluszczykk.training
+
+/**
+Zadeklaruj 5 zmiennych i przypisz do nich adekwatne wartości
+1. Imię
+2. Wiek
+3. Czy ma dzieci
+4. Stała grawitacyjna
+5. Lista lat przestepnych ktore mineły od 2001
+
+ **/
+
+fun main(){
+
+}
