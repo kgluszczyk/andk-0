76a417a6d58350197de78181d4a3dbfc9137a2bf
Krzysztof Gluszczyk <krzysztof.gluszczyk@gmail.com>
2023-02-07 15:21:19 +0100

    Declarative vs imperative - solution

--- playgroundK/src/main/java/com/gluszczykk/training/impertative_vs_declarative/Validation.kt
+++ playgroundK/src/main/java/com/gluszczykk/training/impertative_vs_declarative/Validation.kt
@@ -7,25 +7,7 @@ package com.gluszczykk.training.impertative_vs_declarative
  */
 
 fun List<Input>.validate(): List<ValidationError> {
-    val result = mutableListOf<Input>()
-    for (i in indices) {
-        if (this[i].mandatory) {
-            result.add(this[i])
-        }
-    }
-    val result2 = mutableListOf<Input>()
-    for (i in result.indices) {
-        if (result[i] is InputCheckbox) {
-            if (result[i].value?.fromApiBoolean() == true) {
-                result2.add(this[i])
-            }
-        } else if (result[i].validation != null) {
-            result2.add(this[i])
-        }
-    }
-    val result3 = mutableListOf<ValidationError>()
-    for (i in result2.indices) {
-        result3.add(ValidationError(result2[i].id, "Required"))
-    }
-    return result3
+    return this
+        .filter { it.mandatory && (it is InputCheckbox && it.value?.fromApiBoolean() == true || it.validation != null) }
+        .map { ValidationError(it.id, "Required") }
 }
